0b8dfddb7c22267a8fbfaa8a559c12723daee70a
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-13 22:17:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,18 +16,19 @@ def find_min_in_nested_arrays(src)
 #     row_index += 1
 #   end 
 # end
-  outer_results = []
+    outer_results = []
   row_index = 0
-    while row_index < src.count do
-      element_index = 0
-      while element_index < src[row_index].count do
-        if src[row_index][element_index].min?
-          p src[row_index][element_index]
-        end
-        element_index += 1
+  while row_index < spice_rack.count do
+    element_index = 0
+    while element_index < spice_rack[row_index].count do
+      # How to read the following line of code:
+      #   Array at row_index
+      #   Element of the inner array at element_index
+      #   The first character of that element...
+      if spice_rack[row_index][element_index][0] == "P"
+        outer_results << spice_rack[row_index][element_index]
       end
-      row_index += 1
-    end 
-    outer_results
+      element_index += 1
+    end
+    row_index += 1
   end
-end
