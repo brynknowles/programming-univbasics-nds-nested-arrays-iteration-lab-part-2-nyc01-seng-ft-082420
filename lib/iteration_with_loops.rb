6a1659ef5e723612d292202ae2795e1600c179df
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-13 23:06:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,3 +8,8 @@ while row_index < spice_rack.count do
   element_index = 0
   longest_string_element = ""
   while element_index < spice_rack[row_index].count do
+if spice_rack[row_index][element_index].length > longest_string_element.length
+      longest_string_element = spice_rack[row_index][element_index]
+    end
+    element_index += 1
+  end
